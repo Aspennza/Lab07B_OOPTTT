687896f3a3bcc5d6b4c7dfc5b306a6f4df414b51
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-24 16:23:54 -0400

    In TTTGame, have begun transferring win checking methods from the original game and changing them to suit this new structure

--- src/TTTGame.java
+++ src/TTTGame.java
@@ -4,8 +4,13 @@ import java.awt.*;
 
 public class TTTGame
 {
+    //An int for setting the permanent row count of the board array
+    private static final int ROW = 3;
+    //An int for setting the permanent column count of the board array
+    private static final int COL = 3;
     private static String player;
     private static JFrame frame;
+    private static TTTBoard board;
     private static int moveCnt;
     private static final int MOVES_FOR_WIN = 5;
     private static final int MOVES_FOR_TIE = 7;
@@ -15,7 +20,63 @@ public class TTTGame
     {
         frame = new JFrame();
         generateFrame();
+        board = new TTTBoard();
+    }
 
+    /**
+     * A method for checking if the current player has achieved a win.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether a win was achieved
+     */
+    private static boolean isWin(String player)
+    {
+        //An algorithm for running through possible win conditions
+        if(isColWin(player) || isRowWin(player) || isDiagnalWin(player))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * A method for checking whether the current player has achieved a column win.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether a column win was achieved
+     */
+    private static boolean isColWin(String player)
+    {
+        // checks for a col win for specified player
+        for(int col=0; col < COL; col++)
+        {
+            if(board.getBoard()[0][col].equals(player) &&
+                    board.getBoard()[1][col].equals(player) &&
+                    board.getBoard()[2][col].equals(player))
+            {
+                return true;
+            }
+        }
+        return false; // no col win
+    }
+
+    /**
+     * A method for checking if the current player has achieved a row win.
+     * @param player the current player (X or O)
+     * @return a boolean representing whether a row win was achieved
+     */
+    private static boolean isRowWin(String player)
+    {
+        // checks for a row win for the specified player
+        for(int row=0; row < ROW; row++)
+        {
+            if(board.getBoard()[row][0].equals(player) &&
+                    board.getBoard()[row][1].equals(player) &&
+                    board.getBoard()[row][2].equals(player))
+            {
+                return true;
+            }
+        }
+        return false; // no row win
     }
 
     public static void generateFrame()
